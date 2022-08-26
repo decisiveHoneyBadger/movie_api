d09440b1478a6d394fb50c24159db6f74d9c971b
decisiveHoneyBadger <k.guevenc@web.de>
2022-08-27 00:08:54 +0200

    added cors allowed origin domains

--- index.js
+++ index.js
@@ -13,7 +13,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+
+let allowedOrigins = [
+  'http://localhost:8080',
+  'http://localhost:4200',
+  'http://localhost:1234',
+  'https://desolate-basin-26751.herokuapp.com',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  }),
+);
 
 // imports the auth.js file into the project
 let auth = require('./auth')(app);
