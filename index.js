0bb93c1ea7b5a521ae14b3a9115a649a4976159b
decisiveHoneyBadger <k.guevenc@web.de>
2022-08-27 00:46:26 +0200

    changed cors accesses

--- index.js
+++ index.js
@@ -19,21 +19,28 @@ let allowedOrigins = [
   'http://localhost:4200',
   'http://localhost:1234',
   'https://desolate-basin-26751.herokuapp.com',
+  'http://localhost:1234',
 ];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   }),
+// );
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
+    origin: '*',
   }),
 );
 
