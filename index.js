612a58222929fee34bf25af43dc7786f3e85650a
decisiveHoneyBadger <k.guevenc@web.de>
2022-08-28 00:19:06 +0200

    added new url address to allowed origins

--- index.js
+++ index.js
@@ -20,29 +20,30 @@ let allowedOrigins = [
   'http://localhost:1234',
   'https://desolate-basin-26751.herokuapp.com',
   'http://localhost:1234',
+  'https://decisivehoneybadger.github.io/myFlix-Angular-client',
 ];
-
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         // If a specific origin isn’t found on the list of allowed origins
-//         let message =
-//           'The CORS policy for this application doesn’t allow access from origin ' +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   }),
-// );
 
 app.use(
   cors({
-    origin: '*',
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
   }),
 );
+
+// app.use(
+//   cors({
+//     origin: '*',
+//   }),
+// );
 
 // imports the auth.js file into the project
 let auth = require('./auth')(app);
